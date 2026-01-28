46f4a0aa4620909d46ced3e6a0275502baa9e456
harshanjaswanth-oss <Jaswanth902>
2026-01-28 15:35:36 +0530

    average

---
+++ average.py
@@ -0,0 +1,8 @@
+tree1=int(input("enter the height"))
+tree2=int(input("enter the height"))
+tree3=int(input("enter the height"))
+tree4=int(input("enter the height"))
+tree5=int(input("enter the height"))
+sumis=tree1+tree2+tree3+tree4+tree5
+average=sumis/5
+print("average Height",average)
