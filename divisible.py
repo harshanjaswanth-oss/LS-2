d48fdb8f9d8af6b13948e1a17cfce075714cc2f1
harshanjaswanth-oss <Jaswanth902>
2026-02-11 15:27:06 +0530

    divisible

---
+++ divisible.py
@@ -0,0 +1,7 @@
+num=int(input("enter a number"))
+
+den=int(input("enter a number"))
+if num%den==0:
+    print("nummerator is divisible by denminator")
+else:
+     print("numerator is not divisble by denominator")
