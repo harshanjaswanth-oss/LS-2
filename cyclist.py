95f9ee3d68aee64982c417fa0b35633b75564cd3
harshanjaswanth-oss <Jaswanth902>
2026-02-11 15:43:27 +0530

    cyclist

---
+++ cyclist.py
@@ -0,0 +1,16 @@
+cyclist1=10
+cyclist2=20
+cyclist3=30
+
+avg=(cyclist1+cyclist2+cyclist3)/3
+
+print("avg",avg)
+
+if cyclist1<avg:
+
+   print("cyclist1 is slower")
+if cyclist2<avg:
+   print("cyclist2 is slower")
+
+if cyclist3<avg:
+   print("cyclist3 is slower")
